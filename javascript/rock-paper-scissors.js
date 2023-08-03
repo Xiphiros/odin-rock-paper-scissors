42c71634a0341c5b4df98a92e690c2865a95009b
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2023-08-03 06:56:05 -0400

    Add function to generate computer's choice

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -7,6 +7,7 @@
 
 
     Generate computer's choice
+    Generate random number between 1 and 3
 
     Compare user's choice with the computer's choice
     Return the winner of the game
@@ -17,6 +18,8 @@
 alert("This is rock paper scissors!")
 alert("After this message, input your choice to be played against the computer!")
 let playerChoice = prompt()
+let computerChoice = null
+
 
 function verifyPlayerChoice() {
     playerChoice = playerChoice.toLowerCase()
@@ -37,7 +40,20 @@ function verifyPlayerChoice() {
     }
 }   
 
-console.log(verifyPlayerChoice())
-    
+function getComputerChoice() {
+    let min = Math.ceil(1)
+    let max = Math.floor(3);
+    let randomNumber = Math.floor(Math.random() * (max- min + 1) + min);
 
- 
+    switch(randomNumber) {
+        case 1:
+            return computerChoice = "rock";
+            break;
+        case 2:
+            return computerChoice = "paper";
+            break;
+        case 3:
+            return computerChoice = "scissors";
+            break;
+    }
+}
