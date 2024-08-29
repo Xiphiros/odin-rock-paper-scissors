ce25fd0810697db9b5bbbb3db04020ea27df93ad
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2024-08-28 20:19:29 -0400

    Add getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -68,3 +68,15 @@ function getHumanChoice() {
             break;
     }
 }
+
+function getComputerChoice() {
+    let randomNumber = Math.random * 3;
+
+    if (randomNumber < 1) {
+        return "rock";
+    } else if (randomNumber < 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
